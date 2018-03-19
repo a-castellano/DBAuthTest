fd03063aa6b59585111d50f92f76fdddc0691bc8
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-03-19 17:50:00 +0100

    Create a controller and view

---
+++ lib/DBAuthTest/Controller/AuthUsers.pm
@@ -0,0 +1,47 @@
+package DBAuthTest::Controller::AuthUsers;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+DBAuthTest::Controller::AuthUsers - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched DBAuthTest::Controller::AuthUsers in AuthUsers.');
+}
+
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+azas,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
