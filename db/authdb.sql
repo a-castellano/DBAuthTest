937f66010b196bf4e688745c0eb228603120ac8e
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-03-19 14:12:22 +0100

    Alter table, add NOT NULL

--- db/authdb.sql
+++ db/authdb.sql
@@ -1,16 +1,16 @@
 CREATE TABLE roles (
     id            INTEGER PRIMARY KEY AUTOINCREMENT,
-    role          TEXT UNIQUE
+    role          TEXT NOT NULL UNIQUE
 );
 CREATE TABLE users (
     id            INTEGER PRIMARY KEY AUTOINCREMENT,
-    username      TEXT UNIQUE,
-    email         TEXT,
-    password      TEXT,
-    last_modified DATETIME
+    username      TEXT NOT NULL UNIQUE,
+    email         TEXT NOT NULL,
+    password      TEXT NOT NULL,
+    last_modified DATETIME NOT NULL
 );
 CREATE TABLE user_roles (
-    user_id       INTEGER REFERENCES users(id),
-    role_id       INTEGER REFERENCES roles(id),
+    user_id       INTEGER NOT NULL REFERENCES users(id),
+    role_id       INTEGER NOT NULL REFERENCES roles(id),
     PRIMARY KEY(user_id, role_id)
 );
